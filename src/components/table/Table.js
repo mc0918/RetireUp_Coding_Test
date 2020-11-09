6d25a2aa37640b314939560d9308a92a3c4f266d
Michael Campbell <mcampbell0918@gmail.com>
2020-11-09 15:35:10 -0600

    tests

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -11,7 +11,9 @@ import * as returns from "../../assets/returns.json";
  plus, static imports that are small mean better performance according to React docs
 */
 
-//will take another argument for start year that user wants
+//doing this in useEffect() sometimes causes Table to render in descending order first
+const sortedReturns = returns.default.reverse();
+
 function calculateCumulativeReturn(tableData) {
   let initialReturn = parseFloat(tableData[0].totalReturn);
   let currentReturn;
@@ -19,12 +21,11 @@ function calculateCumulativeReturn(tableData) {
 
   return tableData.map((d) => {
     currentReturn = parseFloat(d.totalReturn);
-    cumulativeReturn = (
-      Math.abs(((currentReturn - initialReturn) / initialReturn) *
-      100
-    )).toFixed(2);
-    if(currentReturn < initialReturn){
-        cumulativeReturn = -cumulativeReturn
+    cumulativeReturn = Math.abs(
+      ((currentReturn - initialReturn) / initialReturn) * 100
+    ).toFixed(2);
+    if (currentReturn < initialReturn) {
+      cumulativeReturn = -cumulativeReturn;
     }
     return { ...d, cumulativeReturn: cumulativeReturn };
   });
@@ -61,7 +62,7 @@ export const Table = () => {
   };
   // empty array passed as second argument so useEffect only runs once in case of multiple renders (S&P 500 Total Returns won't change)
   useEffect(() => {
-    const r = calculateCumulativeReturn(returns.default.reverse());
+    const r = calculateCumulativeReturn(sortedReturns);
     setYearRange("setMinYear", 1926);
     setYearRange("setMaxYear", r[r.length - 1].year);
     setRows(r);
@@ -85,7 +86,7 @@ export const Table = () => {
   };
 
   const renderTableHeader = () => {
-    // this should be dynamically generated if everything else is... 
+    // this should be dynamically generated if everything else is...
     let header = ["Year", "Total Return", "Cumulative Return (%)"];
     return header.map((label) => {
       return <th key={label}>{label}</th>;
@@ -95,6 +96,7 @@ export const Table = () => {
   return (
     <div className="flex-grid">
       <div className="flex-column">
+        <label>{"S&P 500 Returns By Year"}</label>
         <table id="returns">
           <tbody>
             <tr>{renderTableHeader()}</tr>
--- src/components/table/Table.test.js
+++ src/components/table/Table.test.js
@@ -1,5 +1,39 @@
-// TEST CASES
-//1. slider and table render
-//2. updating slider updates table
-//3. cumulative return updates when table does
-//4. state is what we want on initial render
+import { render, screen } from "@testing-library/react";
+import { Table } from "./Table";
+
+describe("Table", () => {
+  test("All table rows appear on initial render", () => {
+    render(<Table />);
+
+    const firstRow = screen.getByText(/1926/i);
+    const lastRow = screen.getByText(/2019/i);
+    expect(firstRow).toBeInTheDocument();
+    expect(lastRow).toBeInTheDocument();
+  });
+
+  test("Table rows sorted in ascending order", () => {
+    const { container } = render(<Table />);
+    const tableHtml = container.querySelectorAll("tr");
+
+    const firstYear = tableHtml[1].querySelectorAll("td")[0].innerHTML;
+    expect(firstYear).toEqual("1926");
+
+    const lastYear = tableHtml[tableHtml.length - 1].querySelectorAll("td")[0]
+      .innerHTML;
+    expect(lastYear).toEqual("2019");
+  });
+});
+
+describe("Range Slider", () => {
+  test("slider appears on initial render", () => {
+    expect(true).toBeFalsy();
+  });
+
+  test("Adjusting slider changes table to show specified years", () => {
+    expect(true).toBeFalsy();
+  });
+
+  test("Adjusting slider causes cumulative returns to update", () => {
+    expect(true).toBeFalsy();
+  });
+});
